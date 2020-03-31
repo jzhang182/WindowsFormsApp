3220c64ca5b33dbbc901bb837c870dc213da3ba5
jzhang182 <1172020845@qq.com>
2020-03-31 17:20:16 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public class Form2: Form
+    {
+        public Button button1;
+        public Form2()
+        {
+            button1 = new Button();
+            button1.Size = new Size(80, 40);
+            button1.Location = new Point(30, 30);
+            button1.Text = "Click me";
+            this.Controls.Add(button1);
+            button1.Click += new EventHandler(button1_Click);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("I love U");
+        }
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form2());
+        }
+    }
+}
